693228baa6268794177cc64b89a723a1e0a45a0a
marco-m24 <68120375+marco-m24@users.noreply.github.com>
2020-10-27 11:56:12 +0100

    New File
    New File

---
+++ wc_move_tab.php
@@ -0,0 +1,3 @@
+// Spostamento delle tab
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60 );
